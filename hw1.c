dbf43cc9d88505a4743e2951c2146416036c45d5
Alekh Meka <ameka4@uic.edu>
2020-01-28 17:48:34 -0600

    modified hw1.h and hw1.c

--- hw1.c
+++ hw1.c
@@ -3,10 +3,10 @@
 
 int main(int argc,char* argv[]){
 
-int static to_compile;
-int static has_a_bunch_of;
-int static ridiculous;
+int static to_compile = 2;
+int static has_a_bunch_of = 3;
+int static ridiculous = 5;
 int static symbols[2];
-printf("ameka4\n");
+printf("ameka4");
   return 0;
 }
