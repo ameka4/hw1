c3e9e09c0fff0a23c140498594f26dc355a6ab5c
Alekh Meka <ameka4@uic.edu>
2020-01-28 17:26:57 -0600

    modified hw1.h and hw1.c

--- hw1.c
+++ hw1.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "hw1.h"
-
-
 
 int main(int argc,char* argv[]){
 
+int to_compile;
+int has_a_bunch_of;
+int ridiculous;
+int symbols[2];
 printf("ameka4\n");
   return 0;
 }
